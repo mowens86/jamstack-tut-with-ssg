720d7b56c1f9117a8d75489def6f0800ee9405de
Mike Owens <mocoding86@gmail.com>
2020-10-10 14:20:19 -0700

    Rewrites

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,13 +1,30 @@
 // jshint esversion: 6
 
 const axios = require("axios");
+const countries = require("./countries.json");
+
 require('dotenv').config();
 
+async function getNews(country) {
+  try {
+    const response = await axios.get(`http://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`);
+    
+    return {  
+      "country": country,
+      "articles": response.data.articles
+    };
+  
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 module.exports = async function() {
-    try {
-      const response = await axios.get(`http://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`);
-      return response.data;
-    } catch (error) {
-      console.error(error);
-    }
+
+  const newPromises = countries.map(getNews);
+  return Promise.all(newPromises).then( newsObjects => {
+    console.log('newsObjects:', newsObjects);
+    return [].concat.apply([], newsObjects);
+  });
+
   };
